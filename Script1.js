3e44e067acd2a4b4cbeeaebffea6fcbacd6dc429
gunel2707 <gunel.muradova2707@gmail.com>
2022-08-29 10:44:33 +0300

    first commit

---
+++ Script1.js
@@ -0,0 +1,130 @@
+// JavaScript source code
+$('.banner-carousel').owlCarousel({
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 3,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: false
+        }
+    }
+
+}
+)
+
+
+/**********************FILTER********************** */
+
+// filterSelection("all")
+// function filterSelection(c) {
+//     var x, i;
+//     x = document.getElementsByClassName("filterDiv");
+//     if (c == "all") c = "";
+//     for (i = 0; i < x.length; i++) {
+//         w3RemoveClass(x[i], "show");
+//         if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+//     }
+// }
+
+// function w3AddClass(element, name) {
+//     var i, arr1, arr2;
+//     arr1 = element.className.split(" ");
+//     arr2 = name.split(" ");
+//     for (i = 0; i < arr2.length; i++) {
+//         if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+//     }
+// }
+
+// function w3RemoveClass(element, name) {
+//     var i, arr1, arr2;
+//     arr1 = element.className.split(" ");
+//     arr2 = name.split(" ");
+//     for (i = 0; i < arr2.length; i++) {
+//         while (arr1.indexOf(arr2[i]) > -1) {
+//             arr1.splice(arr1.indexOf(arr2[i]), 1);
+//         }
+//     }
+//     element.className = arr1.join(" ");
+// }
+
+// // Add active class to the current button (highlight it)
+// var btnContainer = document.getElementById("myBtnContainer");
+// var btns = btnContainer.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function () {
+//         var current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// }
+
+function allFunction() {
+    document.querySelector(".all-btn").classList.add("active");
+    document.querySelector(".meyve-btn").classList.remove("active");
+    document.querySelector(".terevez-btn").classList.remove("active");
+    document.querySelector(".taxil-btn").classList.remove("active");
+    document.querySelector(".other-btn").classList.remove("active");
+
+    document.querySelector(".meyve-row").style.display = "flex";
+    document.querySelector(".terevez-row").style.display = "flex";
+    document.querySelector(".taxil-row").style.display = "flex";
+    document.querySelector(".other-row").style.display = "flex";
+}
+function meyveFunction() {
+    document.querySelector(".meyve-btn").classList.add("active");
+    document.querySelector(".all-btn").classList.remove("active");
+    document.querySelector(".terevez-btn").classList.remove("active");
+    document.querySelector(".taxil-btn").classList.remove("active");
+    document.querySelector(".other-btn").classList.remove("active");
+
+    document.querySelector(".meyve-row").style.display = "flex";
+    document.querySelector(".terevez-row").style.display = "none";
+    document.querySelector(".taxil-row").style.display = "none";
+    document.querySelector(".other-row").style.display = "none";
+}
+function terevezFunction() {
+    document.querySelector(".terevez-btn").classList.add("active");
+    document.querySelector(".all-btn").classList.remove("active");
+    document.querySelector(".meyve-btn").classList.remove("active");
+    document.querySelector(".taxil-btn").classList.remove("active");
+    document.querySelector(".other-btn").classList.remove("active");
+    document.querySelector(".terevez-row").style.display = "flex";
+    document.querySelector(".meyve-row").style.display = "none";
+    document.querySelector(".taxil-row").style.display = "none";
+    document.querySelector(".other-row").style.display = "none";
+
+
+}
+
+
+
+function taxilFunction() {
+    document.querySelector(".taxil-btn").classList.add("active");
+    document.querySelector(".all-btn").classList.remove("active");
+    document.querySelector(".terevez-btn").classList.remove("active");
+    document.querySelector(".meyve-btn").classList.remove("active");
+    document.querySelector(".other-btn").classList.remove("active");
+    document.querySelector(".taxil-row").style.display = "flex";
+    document.querySelector(".meyve-row").style.display = "none";
+    document.querySelector(".terevez-row").style.display = "none";
+    document.querySelector(".other-row").style.display = "none";
+}
+function otherFunction() {
+    document.querySelector(".other-btn").classList.add("active");
+    document.querySelector(".all-btn").classList.remove("active");
+    document.querySelector(".terevez-btn").classList.remove("active");
+    document.querySelector(".taxil-btn").classList.remove("active");
+    document.querySelector(".meyve-btn").classList.remove("active");
+    document.querySelector(".other-row").style.display = "flex";
+    document.querySelector(".meyve-row").style.display = "none";
+    document.querySelector(".terevez-row").style.display = "none";
+    document.querySelector(".taxil-row").style.display = "none";
+}
